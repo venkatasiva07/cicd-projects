0d74935d8c1874ee46545c2397a1c835446edf2a
Bhavanam Venkata Siva Reddy <123177513+venkatasiva07@users.noreply.github.com>
2025-10-16 17:27:32 +0530

    Fix capitalization in heading for environments

--- multi-env-deploy/src/App.js
+++ multi-env-deploy/src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div>
       <h1>Hello, React CI/CD Pipeline 🚀</h1>
       <h2>Testing GitHub Actions Workflow</h2>
-      <h3>Trying to deploy the app on Multiple different Environments</h3>
+      <h3>Trying to deploy the app on Multiple Different Environments </h3>
     </div>
   );
 }
