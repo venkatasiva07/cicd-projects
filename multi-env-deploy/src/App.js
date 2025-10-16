4e9c6844a871d968cf95adadce4cf9e3c72b920d
Bhavanam Venkata Siva Reddy <123177513+venkatasiva07@users.noreply.github.com>
2025-10-16 21:20:26 +0530

    secondcommit

--- multi-env-deploy/src/App.js
+++ multi-env-deploy/src/App.js
@@ -4,7 +4,7 @@ function App() {
       <h1>Hello, React CI/CD Pipeline 🚀</h1>
       <h2>Testing GitHub Actions Workflow</h2>
       <h3>Trying to deploy the app on Multiple Different Environments </h3>
-      <h4>First it will deploy automatically on to dev server , After you have to approve manually for deployment</h4>
+      <h4>First it will deploy automatically on to dev server ,After you have to approve manually for deployment</h4>
     </div>
   );
 }
