4fb8cb5dbd77fd05005a6bb5b1c43164646155b3
venkatasiva07 <sivavenkata0007@gmail.com>
2025-11-22 16:26:29 +0530

    k8s-deployment8

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -4,6 +4,7 @@ function App() {
       <h1>Hello, React CI/CD!</h1>
       <h2> i am tying to test the cicd using github actions</h2>
       <h3> now i deploying into k8s with ubuntu user</h3>
+      <h4> erors </h4>
     </div>
   );
 }
